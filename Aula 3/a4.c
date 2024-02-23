0555c2165b37ab59bb78847c9c535b034a089a44
Julia Silva <juliasilvass01@gmail.com>
2024-02-23 17:24:54 -0300

    feat: completando o exericio 4 aula 3

--- Aula 3/a4.c
+++ Aula 3/a4.c
@@ -9,18 +9,30 @@ M C D U
 
 int main() {
     int num = 0, vetor[4];
+    char uni[4] = {'M', 'C', 'D', 'U'};
 
     printf("Digite um número de 4 dígitos: ");
     scanf("%d", &num);
 
     if (num <= 9999){  
-
-        vetor[0] = num /1000;
+        vetor[0] = num/1000;
+        vetor[1] = (num/100)%10;
+        vetor[2] = (num/10)%10;
+        vetor[3] = num%10;
+    } else {
+        printf("Tem mais de 4 dígitos!");
+    }
+    
+    for (int i = 0; i < 4; i++){
+        printf("%d \t", vetor[i]);
         
-        
+        if (i == 3){
+            printf("\n");
+            for (int j = 0; j < 4; j++){
+                printf("%c \t", uni[j]);
+            }
+        }
     }
 
-
-    
-
     return 0;
+}
